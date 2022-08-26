a5e06e93b3466e088701ccbf811c240f1c79a3d7
HP Harmsen <hph@oberon.nl>
2022-08-26 16:27:41 +0200

    added get_simplicate_worklogs

--- jirasimp/__init__.py
+++ jirasimp/__init__.py
@@ -1,3 +1,3 @@
 from .jira_part import Jira, get_data_from_jira, get_jira_worklogs
-from .simplicate import simplicate_hours
+from .simplicate import simplicate_hours, get_simplicate_worklogs
 from .utilities import month_in_weeks, format_item
